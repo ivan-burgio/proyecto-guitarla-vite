b909e5ae261caa568ff5dfde1b4aa2b04a230a42
ivan-burgio <ivan.24.burgio@gmail.com>
2024-05-28 11:46:50 -0300

    comienzo reducer

---
+++ src/reducers/cart-reducer.ts
@@ -0,0 +1,54 @@
+import { db } from "../data/db";
+import { CartItem, Guitar } from "../types";
+
+export type CartActions =
+    | { type: "add-to-cart"; payload: { item: Guitar } }
+    | { type: "remove-from-cart"; payload: { id: Guitar["id"] } }
+    | { type: "decrease-quantity"; payload: { id: Guitar["id"] } }
+    | { type: "increase-quantity"; payload: { id: Guitar["id"] } }
+    | { type: "clear-cart" };
+
+export type CartState = {
+    data: Guitar[];
+    cart: CartItem[];
+};
+
+export const initialState : CartState = {
+    data: db,
+    cart: [],
+}
+
+export const cartReducer = (
+    state: CartState = initialState,
+    action: CartActions
+) => {
+    if(action.type === "add-to-cart") {
+        return {
+            ...state
+        }
+    }
+
+    if(action.type === "remove-from-cart") {
+        return {
+            ...state
+        }
+    }
+
+    if(action.type === "decrease-quantity") {
+        return {
+            ...state
+        }
+    }
+
+    if(action.type === "increase-quantity") {
+        return {
+            ...state
+        }
+    }
+
+    if(action.type === "clear-cart") {
+        return {
+            ...state
+        }
+    }
+}
